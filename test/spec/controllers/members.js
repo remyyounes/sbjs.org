2319ab0fbf5526f82f4c33da9e7b95518fa55523
Jim McGaw <smoochy@Jim-McGaws-MacBook-Pro.local>
2013-08-01 17:54:51 -0700

    screw specs for now, get code architecture stable then spec stuff

--- test/spec/controllers/members.js
+++ test/spec/controllers/members.js
@@ -18,37 +18,4 @@ describe('Controller: MembersCtrl', function () {
 
   }));
 
-  it("has members defined", function() {
-    expect(scope.members).toBeDefined();
-  });
-
-  describe("#parseMembers", function() {
-    it("parses owner objects out of result data", function() {
-      expect(scope.members).not.toBeDefined();
-      var result = {
-        data : [
-          { owner : "Jim" },
-          { owner : "Remy" }
-        ]
-      };
-      scope.parseMembers(result);
-      expect(scope.members).toEqual(["Jim", "Remy"]);
-    });
-  });
-
-  describe("#fetchMembers", function() {
-    it("fetches members from github URL", function() {
-      cookies.token = "1j2j3j475hf7";
-      var thenSpy = jasmine.createSpy("then");
-      getSpy.andReturn({
-        then: thenSpy
-      });
-      var url = 'https://api.github.com/repos/sbjs/sbjs.profiles/forks?access_token=' + cookies.token;
-
-      scope.fetchMembers();
-
-      expect(http.get).toHaveBeenCalledWith(url);
-      expect(thenSpy).toHaveBeenCalledWith(scope.parseMembers);
-    });
-  });
 });
